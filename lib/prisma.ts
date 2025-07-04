04a35216892d4a959aaa3f4fd3721f839b897146
marceoroq <oroquieta.m@gmail.com>
2025-07-04 14:28:15 -0300

    chore: prepare prisma for future deploy in Vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,26 @@
 import { PrismaClient } from '../lib/generated/prisma'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
+import { neonConfig, Pool, PoolConfig } from '@neondatabase/serverless'
+import { PrismaNeon } from '@prisma/adapter-neon';
+import ws from 'ws';
+
+// Sets up WebSocket connections, which enables Neon to use WebSocket communication.
+neonConfig.webSocketConstructor = ws;
+
+// Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+
+// Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
+const adapter = new PrismaNeon(pool as PoolConfig);
+
 const globalForPrisma = global as unknown as { 
     prisma: PrismaClient
 }
 
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+// We add the adapter to the PrismaClient
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter }).$extends(withAccelerate())
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
