ec0cc92ea9c513ad09dfd7c14c0fe8fdd0e22c9b
marceoroq <oroquieta.m@gmail.com>
2025-07-03 22:34:35 -0300

    comment(Serializer): add comment for future reference

--- lib/serializer/product.serializer.ts
+++ lib/serializer/product.serializer.ts
@@ -2,6 +2,14 @@ import { toPlainObject } from "@/lib/utils";
 import { Product as PrismaProduct } from "@/lib/generated/prisma";
 import { Product as PlainProduct } from "@/types";
 
+/**
+ * These serializations are performed because the objects returned by Prisma use the Decimal
+ * data types that we define in our schema. That doesn't exist in JavaScript, so we have to 
+ * transform them into some JS primitive. To maintain precision, Decimals are transformed to 
+ * strings (in the case of rating, since precision is not as important, we pass it directly to
+ * a number) besides, we remove other properties, methods and functions by making a more generic 
+ * serialization with toPlainObject.
+ */
 export function convertPrismaProductToPOJO(prismaProduct: PrismaProduct): PlainProduct {
   return {
     ...toPlainObject(prismaProduct),
