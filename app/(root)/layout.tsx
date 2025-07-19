23355a39ae5b01b52ee94217374326ef1ad34702
marceoroq <oroquieta.m@gmail.com>
2025-07-19 11:58:57 -0300

    remove unneccessary force-dinamic

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,7 @@
 import Footer from "@/components/shared/footer";
 import Header from "@/components/shared/header";
 
-export const dynamic = "force-dynamic";
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col">
       <Header />
